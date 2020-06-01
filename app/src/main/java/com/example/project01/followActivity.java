69089fa89c70e12657bed3c7fb0e079d7b9cff03
chen9601 <chpark0641@gmail.com>
2020-06-01 20:44:48 +0900

    사진 저장

--- app/src/main/java/com/example/project01/followActivity.java
+++ app/src/main/java/com/example/project01/followActivity.java
@@ -37,10 +37,13 @@ import androidx.core.content.ContextCompat;
 
 import com.example.project01.predictivemodels.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class followActivity extends AppCompatActivity {
@@ -55,9 +58,13 @@ public class followActivity extends AppCompatActivity {
     ImageView imageview;
 
     ImageView reset;
+    ImageView save;
     TensorFlowClassifier classifier;
     private SquareImageView faceImageView;
     PhotoCard p;
+    Bitmap b;
+    String filename;
+    int count=0;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -70,6 +77,7 @@ public class followActivity extends AppCompatActivity {
         setContentView(R.layout.follow);
 
         // 관리 권한 획득
+        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         if(permissionCheck == PackageManager.PERMISSION_GRANTED){
             Toast.makeText(this,"Read Storage Permission Granted",Toast.LENGTH_LONG).show();
@@ -104,6 +112,16 @@ public class followActivity extends AppCompatActivity {
                 Capture();
             }
         });
+
+        save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveImage(b,filename);
+            }
+        });
+
+        save.setEnabled(false);
 
         reset=findViewById(R.id.reset);
         reset.setOnClickListener(new View.OnClickListener() {
@@ -145,10 +163,7 @@ public class followActivity extends AppCompatActivity {
         });
     }
 
-    public void Reset()
-    {
-        faceImageView.setVisibility(View.GONE);
-    }
+
 
     private void detectEmotion(Bitmap bitmap){
 
@@ -180,7 +195,15 @@ public class followActivity extends AppCompatActivity {
         try{
             final Classification res = classifier.recognize(normalized_pixels);
             //if it can't classify, output a question mark
-            Toast.makeText(this.getApplicationContext(),res.getLabel(),Toast.LENGTH_SHORT).show();
+
+            filename=res.getLabel();
+
+            if(check(res.getLabel(),p.emotion))
+                Toast.makeText(this.getApplicationContext(),"정답입니다",Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this.getApplicationContext(),"틀렸습니다",Toast.LENGTH_SHORT).show();
+
+
             }
         catch (Exception e){
             System.out.print("Exception:"+e.toString());
@@ -192,8 +215,61 @@ public class followActivity extends AppCompatActivity {
         Bitmap rotatedBitmap = Bitmap.createBitmap(grayImage, 0, 0, grayImage.getWidth(), grayImage.getHeight(), matrix, true);
 
 
+        b=rotatedBitmap;
+        save.setEnabled(true);
+
+
         this.faceImageView.setImageBitmap(rotatedBitmap);
         faceImageView.setVisibility(View.VISIBLE);
+    }
+
+
+    private void saveImage(Bitmap finalBitmap, String image_name) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root);
+        myDir.mkdirs();
+        String fname = "Image-" + image_name+ count+".jpg";
+        File file = new File(myDir, fname);
+        while (file.exists())
+        {
+            count++;
+            fname = "Image-" + image_name+ count+".jpg";
+            file = new File(myDir, fname);
+        }
+        Log.i("LOAD", root + fname);
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Reset()
+    {
+        faceImageView.setVisibility(View.GONE);
+    }
+
+    private boolean check(String s1, String s2)
+    {
+        if(s1.equals("Angry")&&s2.equals("화나다"))
+            return true;
+        else if(s1.equals("Disgust")&&s2.equals("짜증나다"))
+            return true;
+        else if(s1.equals("Fear")&&s2.equals("무섭다"))
+            return true;
+        else if(s1.equals("Happy")&&s2.equals("즐겁다"))
+            return true;
+        else if(s1.equals("Sad")&&s2.equals("슬프다"))
+            return true;
+        else if(s1.equals("Surprise")&&s2.equals("놀라다"))
+            return true;
+        else if(s1.equals("Neutral")&&s2.equals("무표정"))
+            return true;
+        else return false;
     }
 
     private void loadModel() {
